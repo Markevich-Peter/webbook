7627cd4aa61457a82b7c1b9aea5dd48724e11c29
Peter <mpeter2001@mail.ru>
2018-03-13 12:39:35 +0200

    first

---
+++ src/main/java/Controller/HiController.java
@@ -0,0 +1,7 @@
+package Controller;
+
+/**
+ * Created by Peter on 13.03.18.
+ */
+public class HiController {
+}
---
+++ src/main/java/model/Model.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Created by Peter on 13.03.18.
+ */
+public class Model
+{
+    Model model;
+}
